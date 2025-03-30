bb6ea6cdaf24b8c3abfa38c699bce3d60ed0e24e
Paweł Hase <pawelhase@gmail.com>
2025-03-30 15:02:41 +0200

    Globar tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        plant: {
+          50: "#c2d7b6",
+          100: "#b8d0aa",
+          200: "#adc99d",
+          300: "#a3c391",
+          400: "#99bc85",
+          500: "#8aa978",
+          600: "#7a966a",
+          700: "#7a966a",
+          800: "#6b845d",
+          900: "#5c7150",
+          950: "#4d5e43",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
