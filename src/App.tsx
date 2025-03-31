e925e544ef34eac46eff7eeb9e082ed4f95ef37a
Erneo97 <michalkaniewski1997@gmail.com>
2025-03-31 20:57:41 +0200

    Dodanie klasy użytkownika, rozdzielenie głównego widoku aplikacji w zależności czy użytkownik jest zalogowany. Jeżeli nie to wymusza otwarcie okna logowania

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import Auth from "./components/Auth";
 import NavBar from "./components/NavBar/NavBar";
 import { useAuth } from "./hooks/useAuth";
+import {UserLogin} from "./types/dataUser"
+
+const userLogedIn = new UserLogin("User1234", 15, 8, "example@example.com", true);
 
 function App() {
   const user = useAuth();
@@ -8,10 +11,20 @@ function App() {
   return (
     <>
       <NavBar logoPath="src/assets/logo-text-v2.svg" />
+
+      {userLogedIn ? 
       <div>
-        {user ? <p>Zalogowany jako: {user.email}</p> : <p>Nie zalogowany</p>}
+        <p className="header-text">Strona główna</p>
+        
       </div>
-      <Auth />
+      :
+       <div> 
+        <div>
+        {userLogedIn && <p >Zalogowany jako: {userLogedIn.email}</p>}
+        </div>
+        <Auth />
+      </div>}
+      
     </>
   );
 }
