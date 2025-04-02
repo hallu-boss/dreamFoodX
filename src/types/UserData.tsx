17d9f81c5b4f99986af5c1b107f2ae8e82e02ac9
Erneo97 <michalkaniewski1997@gmail.com>
2025-04-02 23:25:17 +0200

    Uporządkowanie i przeróbki klasy UserData reprezentującej dane użytkownika

---
+++ src/types/UserData.tsx
@@ -0,0 +1,57 @@
+//  Klasa użytkownika korzystajacego z danej chwili z witryny
+
+export class UserData  {
+  // TODO id użytkownika - opcjonalnie
+  name: string;
+  surname: string
+  cookingHours: number;
+  email: string;
+  isLoggedIn: boolean = false;
+
+ 
+
+  constructor (
+        name="Guest",
+        surname ="",
+        email="",
+        cookingHours = 0,
+        isLoggedIn=false
+    ) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.cookingHours = cookingHours;
+    this.isLoggedIn = isLoggedIn;
+  }
+
+  //  METODY
+
+  checkLoginStatus() : boolean {
+    return this.isLoggedIn;
+  }
+
+   login(email: string) : void {
+    this.email = email
+    this.isLoggedIn = true;
+  }
+
+  setDataUser(name: string, surname: string, cookingHours: number) {
+    this.name = name;
+    this.surname = surname;
+    this.cookingHours = cookingHours;
+  }
+
+  logout() : void {
+    this.isLoggedIn = false;
+    this.setGuest();
+  }
+
+  private setGuest() : void {
+    this.name = "Guest"; 
+    this.surname = "";
+    this.email = "";
+    this.cookingHours = 0;
+  }
+}
+
+export default UserData;
