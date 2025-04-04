0f17a1bac7382489ebb752d3f35a2694dfaac789
Paweł Hase <pawelhase@gmail.com>
2025-04-04 23:43:39 +0200

    Installed cors

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const JWT_SECRET = "your_secret_key";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
